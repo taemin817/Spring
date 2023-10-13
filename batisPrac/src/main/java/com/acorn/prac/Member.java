95e89c34f4059f78812b05b65613936736627fab
taemin817 <tmpark11@naver.com>
2023-10-13 16:48:52 +0900

    경로수정

---
+++ batisPrac/src/main/java/com/acorn/prac/Member.java
@@ -0,0 +1,11 @@
+package com.acorn.prac;
+
+import lombok.Data;
+
+@Data
+public class Member {
+
+	String id;
+	String pwd;
+	String name;
+}
---
+++ batisPrac/src/test/java/com/acorn/prac/MemberRepoTest.java
@@ -0,0 +1,32 @@
+package com.acorn.prac;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/test.xml" , "file:src/main/webapp/WEB-INF/spring/**/test2.xml"} )
+@Log4j
+public class MemberRepoTest {
+
+	@Autowired
+	MemberRepo dao;
+	
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		log.info(dao.selectAll().size());
+	}
+	@Test
+	public void test2() {
+		//fail("Not yet implemented");
+		log.info(dao.selectOne("ptm"));
+		//log.info(dao.selectOne("ptm").equals("tmp"));
+	}
+}
